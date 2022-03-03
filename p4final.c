c7104d588f6f1b3448c4fb6c2dca733ee80bcca5
Fariha12t <96039101+Fariha12t@users.noreply.github.com>
2022-03-03 09:38:55 +0000

    struct

--- p4final.c
+++ p4final.c
@@ -1,4 +1,54 @@
-/*
+#include<stdio.h>
+#include<math.h>
+int input_degree()
+{
+  int n;
+  printf("enter degree:\n");
+  scanf("%d",&n);
+  return n;
+}
+float input_x()
+{
+  float x;
+  printf("enter value of x:\n");
+  scanf("%f",&x);
+  return x;
+}
+void input_coefficients(int n,float a[n])
+{
+  for(int i=1;i<=n+1;i++)
+    {
+      scanf("%f",&a[i]);
+    }
+}
+float evaluate_polynomial(int n,float a[n],float x)
+{
+  int result=0;
+  for(int i=1;i<=n+1;i++)
+    {
+      for(int n=3;n>0;n--)
+        {
+           result=result+a[i]*pow(x,n);
+          break;
+        }  
+    }
+  return result;
+}
+void out_put(int n,float a[n],float x,float result)
+{
+  printf("%f\n",result);
+}
+int main()
+{
+  int n;
+  float x;
+  n=input_degree();
+  x=input_x();
+  float a[n],result;
+  input_coefficients(n,a);
+  result=evaluate_polynomial(n,a,x);
+  out_put(n,a,x,result);
+}/*
 
 Write a program to find nth number in fibonacci sequence.
 Fibonacci sequence consists of 0,1,1,2,3,5,8,13,21........
